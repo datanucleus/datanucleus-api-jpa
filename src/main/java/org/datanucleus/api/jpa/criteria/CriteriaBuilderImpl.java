83eda460a5f9642587b4b080b2e41dff4f092b9b
andyjefferson <andy@datanucleus.org>
2015-03-23 15:50:23 +0000

    TODOs

--- src/main/java/org/datanucleus/api/jpa/criteria/CriteriaBuilderImpl.java
+++ src/main/java/org/datanucleus/api/jpa/criteria/CriteriaBuilderImpl.java
@@ -1386,9 +1386,9 @@ public class CriteriaBuilderImpl implements CriteriaBuilder, Serializable
      */
     public <C, R> SimpleCase<C, R> selectCase(Expression<? extends C> expr)
     {
-        // TODO Implement this
+        // TODO Implement this. What is the "expr" for?
         throw new UnsupportedOperationException(
-            "CriteriaBuilder.selectCase not yet implemented. Provide a testcase that uses this and raise a JIRA issue attaching your testcase");
+            "CriteriaBuilder.selectCase(Expression) not yet implemented. Do you know what this is supposed to do and can provide an example? Provide a testcase that uses this and raise a JIRA issue attaching your testcase");
     }
 
     /* (non-Javadoc)
